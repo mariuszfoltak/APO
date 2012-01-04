2392428cb75a84dbc1ec7c054c8a3f8825b603c0
Mariusz Foltak <browarrek@gmail.com>
2012-01-04 19:36:47 +0100

    Mozliwe przesuwanie punktow

--- APO/UOPDialog.Designer.cs
+++ APO/UOPDialog.Designer.cs
@@ -30,32 +30,57 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Location = new System.Drawing.Point(11, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(250, 250);
+            this.panel1.Size = new System.Drawing.Size(258, 258);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
             this.panel1.Click += new System.EventHandler(this.panel1_Click);
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 270);
+            this.label1.Location = new System.Drawing.Point(232, 274);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(12, 300);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(191, 299);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
             // UOPDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 346);
+            this.ClientSize = new System.Drawing.Size(279, 347);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "UOPDialog";
@@ -69,5 +94,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
--- APO/UOPDialog.cs
+++ APO/UOPDialog.cs
@@ -12,6 +12,30 @@ namespace APO
     public partial class UOPDialog : Form
     {
         private List<Point> points = new List<Point>();
+        private Graphics graphicsObj;
+        private Point draggingPoint;
+        private bool isDragging = false;
+
+        BackgroundWorker bw = new BackgroundWorker();
+
+        class Point
+        {
+            public int X;
+            public int Y;
+
+            public Point() { }
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public PointF ToPointF()
+            {
+                return new PointF(X, Y);
+            }
+        }
 
         class PointComparer : IComparer<Point>
         {
@@ -31,67 +55,154 @@ namespace APO
         public UOPDialog()
         {
             InitializeComponent();
+            DoubleBuffered = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+        }
+
+        private void clearPanel()
+        {
+            graphicsObj.Clear(panel1.BackColor);
+
+            graphicsObj.DrawLine(Pens.LightGray, 51, 0, 51, 255);
+            graphicsObj.DrawLine(Pens.LightGray, 102, 0, 102, 255);
+            graphicsObj.DrawLine(Pens.LightGray, 153, 0, 153, 255);
+            graphicsObj.DrawLine(Pens.LightGray, 204, 0, 204, 255);
+            graphicsObj.DrawLine(Pens.LightGray, 0, 51, 255, 51);
+            graphicsObj.DrawLine(Pens.LightGray, 0, 102, 255, 102);
+            graphicsObj.DrawLine(Pens.LightGray, 0, 153, 255, 153);
+            graphicsObj.DrawLine(Pens.LightGray, 0, 204, 255, 204);
+        }
+
+        private void drawPanel()
+        {
+            Point a = new Point(0, 255);
+            clearPanel();
+            points.Sort(new PointComparer());
+            int count = points.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                graphicsObj.DrawLine(Pens.Black, a.ToPointF(), points[i].ToPointF());
+                a = points[i];
+                graphicsObj.FillRectangle(Brushes.Black, new Rectangle(a.X - 2, a.Y - 2, 5, 5));
+            }
+
+            graphicsObj.DrawLine(Pens.Black, a.ToPointF(), new Point(255, 0).ToPointF());
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics graphicsObj = panel1.CreateGraphics();
+            graphicsObj = panel1.CreateGraphics();
             Pen myPen = new Pen(System.Drawing.Color.Black, 1);
 
-            graphicsObj.Clear(panel1.BackColor);
+            clearPanel();
 
-            graphicsObj.DrawLine(myPen, 0, 250, 250, 0);
+            graphicsObj.DrawLine(myPen, 0, 255, 255, 0);
         }
 
         private void panel1_Click(object sender, EventArgs e)
         {
-            MouseEventArgs me = (MouseEventArgs)e;
-            if (me.Button == MouseButtons.Right)
-            {
-                for (int i = 0; i < points.Count; i++)
-                {
-                    if (points[i].X - 3 < me.X && points[i].X + 3 > me.X)
-                        if (points[i].Y - 3 < me.Y && points[i].Y + 3 > me.Y)
-                        {
-                            points.Remove(points[i]);
-                            break;
-                        }
-                }
-            }
-            else
-            {
-                points.Add(new Point(((MouseEventArgs)e).X, ((MouseEventArgs)e).Y));
-                points.Sort(new PointComparer());
-            }
-            Point a = new Point(0,250);
+            //MouseEventArgs me = (MouseEventArgs)e;
+            //if (me.Button == MouseButtons.Right)
+            //{
+            //    for (int i = 0; i < points.Count; i++)
+            //    {
+            //        if (points[i].X - 3 < me.X && points[i].X + 3 > me.X)
+            //            if (points[i].Y - 3 < me.Y && points[i].Y + 3 > me.Y)
+            //            {
+            //                points.Remove(points[i]);
+            //                break;
+            //            }
+            //    }
+            //}
+            //else
+            //{
+            //    points.Add(new Point(((MouseEventArgs)e).X, ((MouseEventArgs)e).Y));
+            //    points.Sort(new PointComparer());
+            //}
+            //Point a = new Point(0,255);
 
-            Graphics graphicsObj = panel1.CreateGraphics();
-            Pen myPen = new Pen(System.Drawing.Color.Black, 1);
-            
-            graphicsObj.Clear(panel1.BackColor);
+            //Pen myPen = new Pen(System.Drawing.Color.Black, 1);
 
-            for (int i = 0; i < points.Count; i++)
-            {
-                graphicsObj.DrawLine(myPen, a, points[i]);
-                a = points[i];
-                //graphicsObj.DrawRectangle(myPen, new Rectangle(a, new Size(2, 2)));
-                graphicsObj.FillRectangle(Brushes.Black, new Rectangle(a.X - 2, a.Y - 2, 5, 5));
-            }
-            graphicsObj.DrawLine(myPen, a, new Point(250,0));
+            //clearPanel();
+
+            //for (int i = 0; i < points.Count; i++)
+            //{
+            //    graphicsObj.DrawLine(myPen, a, points[i]);
+            //    a = points[i];
+            //    graphicsObj.FillRectangle(Brushes.Black, new Rectangle(a.X - 2, a.Y - 2, 5, 5));
+            //}
+            //graphicsObj.DrawLine(myPen, a, new Point(255,0));
         }
 
         private void panel1_MouseDown(object sender, MouseEventArgs e)
         {
+            if (e.Button == MouseButtons.Right)
+                return;
             for (int i = 0; i < points.Count; i++)
             {
                 if (points[i].X - 3 < e.X && points[i].X + 3 > e.X)
                     if (points[i].Y - 3 < e.Y && points[i].Y + 3 > e.Y)
                     {
-                        points.Remove(points[i]);
+                        isDragging = true;
+                        draggingPoint = points[i];
                         label1.Text = "Udało się!";
+                        if (bw.IsBusy != true)
+                        {
+                            bw.RunWorkerAsync();
+                        }
                         return;
                     }
             }
         }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                draggingPoint.X = e.X;
+                draggingPoint.Y = e.Y;
+                label1.Text = "X: " + draggingPoint.X.ToString() + " Y: " + draggingPoint.Y.ToString();
+                
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if (points[i].X - 3 < e.X && points[i].X + 3 > e.X)
+                        if (points[i].Y - 3 < e.Y && points[i].Y + 3 > e.Y)
+                        {
+                            points.Remove(points[i]);
+                            drawPanel();
+                            break;
+                        }
+                }
+            }
+            else if (!isDragging)
+            {
+                points.Add(new Point(e.X, e.Y));
+                points.Sort(new PointComparer());
+                drawPanel();
+            }
+            else
+            {
+                isDragging = false;
+            }
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (isDragging)
+            {
+                drawPanel();
+                System.Threading.Thread.Sleep(40);
+            }
+        }
+
+
     }
 }
